5907e71ea01a39f17af5cdc6b52df881394d5ec5
user <user@linux-3rj2.site>
2009-01-22 13:38:45 -0600

    add poc project

---
+++ fulltcm/admin.py
@@ -0,0 +1,8 @@
+from fulltcm.models import PersonStatus,  Regions,  Rols,  Persons
+from django.contrib import admin
+
+admin.site.register(Regions)
+admin.site.register(Rols)
+admin.site.register(PersonStatus)
+admin.site.register(Persons)
+#admin.site.register(Rols)
