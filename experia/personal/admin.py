3fbf82b6293d11e6d571509d851ce3a0b3e74fc0
luis toledo <luisernesto.toledo@gmail.com>
2009-01-29 14:43:03 -0600

    update personal

--- experia/personal/admin.py
+++ experia/personal/admin.py
@@ -5,10 +5,11 @@ from django.contrib import admin
 class PersonAdmin(admin.options.ModelAdmin):
     fieldsets =  [
         (None,              {'fields': ['first_name', 'last_name']}),
-        ('Puesto',          {'fields': ['hiring_date', 'puesto']}),
-        ('Region/Status',   {'fields': ['status', 'region',]}),
-        ('Contacto',        {'fields': ['telephone_home', 'telephone_personal', 'email_1', 'email_2', 'prefered_email']}),
-        ('Otros',           {'fields': ['passport']}),
+        ('Status',          {'fields': ['puesto', 'type', 'status', 'region']}),
+        ('Contratacion',    {'fields': ['hiring_date','fire_date']}),
+        ('Contacto',        {'fields': ['telephone_home', 'telephone_personal', 'email_1', 'email_2', 'prefered_email'] , 'classes': ['collapse']}),
+        ('Otros',           {'fields': ['birthday','passport','visa'], 'classes': ['collapse']}),
+        ('Comentarios',     {'fields': ['coment'], 'classes': ['collapse']}),
     ]
     list_display=('first_name', 'status', 'puesto', 'antiguedad')
     list_filter=('first_name', 'status', 'region', 'puesto')
