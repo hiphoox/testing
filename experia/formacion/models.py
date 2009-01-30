f9cec608af39542c2a6e9b5007a6c80f66ac76f6
luis toledo <luisernesto.toledo@gmail.com>
2009-01-29 19:46:58 -0600

    update full site

--- experia/formacion/models.py
+++ experia/formacion/models.py
@@ -1,32 +1,33 @@
 from django.db import models
 
 class Module(models.Model):
-    name = models.CharField(max_length=10)
-    tags = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
     def __unicode__(self):
         return self.name
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
     module = models.ForeignKey(Module)
-    duration = models.IntegerField('Duracion')
+    duration = models.IntegerField('Duracion', blank=True, null=True)
+    tags = models.CharField(max_length=50, blank=True)
     def __unicode__(self):
-        return self.name
+        return "%s : %s" % (self.module, self.name)
 
-class CourseStatus(models.Model):
-    COURSE_STATUS_CHOICE = (('E', 'En Proceso') ,   ('P','Planeado')    ,   ('C','Cancelado')   , ('F','Finalizado') )
-    name = models.CharField(max_length=1, choices=COURSE_STATUS_CHOICE)
-    def __unicode__(self):
-        return self.name
+#class CourseStatus(models.Model):
+#    COURSE_STATUS_CHOICE = (('E', 'En Proceso') ,   ('P','Planeado')    ,   ('C','Cancelado')   , ('F','Finalizado') )
+#    name = models.CharField(max_length=1, choices=COURSE_STATUS_CHOICE)
+#    def __unicode__(self):
+#        return self.name
 
 class CourseControl(models.Model):
+    COURSE_STATUS_CHOICE = (('E', 'En Proceso') ,   ('P','Planeado')    ,   ('C','Cancelado')   , ('F','Finalizado') )
     course = models.ForeignKey(Course)
     person = models.ManyToManyRel('personal.Person')
     begin_date = models.DateTimeField('start')
-    end_date = models.DateTimeField('end')
-    grade = models.IntegerField(blank='true')
-    duration = models.IntegerField(blank='true')
-    status = models.ForeignKey(CourseStatus)
+    end_date = models.DateTimeField('end', null='true')
+    grade = models.IntegerField(null='true', blank='true')
+    duration = models.IntegerField(blank='true', null='true')
+    status = models.CharField(choices=COURSE_STATUS_CHOICE , max_length=1)
     def __unicode__(self):
         return "%s : %s : %s %s %s" & (self.person , self.course, self.grade, self.status, self.begin_date )
 
@@ -38,6 +39,6 @@ class Topic(models.Model):
 class KnowledgeMatrix(models.Model):
     person = models.ManyToManyField('personal.Person')
     topics = models.ManyToManyField(Topic)
-    coment = models.CharField(max_length=200)
+    coment = models.CharField(max_length=200, blank=True)
     def __unicode__(self):
-        return self.person
+        return "%s - %s : %s" % (self.person, self.topics, self.coment)
